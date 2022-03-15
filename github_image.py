07db7ec695e12b21df3ff481cf50b19780348509
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-03-16 00:14:40 +0900

    Update github_image.py

--- github_image.py
+++ github_image.py
@@ -1,25 +1,40 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
+# Name:         GitHubImageMaker
+# Purpose:      GitHubのグリッド風の画像生成プログラムです
 #
-# Author:      Shaneron
+# Author:       Shaneron
 #
-# Created:     15/03/2022
-# Copyright:   (c) Shaneron 2022
+# Created:      15/03/2022
+# Copyright:    (c) Shaneron 2022
 #-------------------------------------------------------------------------------
 
-import tkinter as tk
-import random
+try:
+    import tkinter as tk
+    import random
+except ModuleNotFoundError as NO_MODULE_ERROR:
+    print(f"ModuleNotFoundError:{NO_MODULE_ERROR}")
 
 class App(tk.Frame):
-    def __init__(self,master):
+    def __init__(self, master):
         super().__init__(master)
         self.pack()
 
+        #メニューバー
+        self.menu_bar = tk.Menu(master)
+        master.config(menu=self.menu_bar)
+
+        # 親メニュー
+        self.menus = tk.Menu(master)
+        self.menu_bar.add_cascade(label="ファイル", menu=self.menus)
+
+        # 子メニュー
+        self.menus.add_command(label="新規作成")
+
         # キャンバス描画
-        self.canvas = tk.Canvas(master, background="white", height=300, width=300)
+        self.canvas = tk.Canvas(master, background="#ffffff", height=300, width=300)
 
         master.after(10, self.draw_canvas)
+
 
     def rands(self):
         x = random.randint(50, 300)
@@ -29,6 +44,7 @@ class App(tk.Frame):
         self.canvas.delete("obj")
         # 図形描画
         self.canvas.create_polygon(self.rands(), self.rands(), self.rands(), self.rands(), self.rands(), self.rands(),fill="red", tags="obj")
+        self.canvas.create_rectangle(100, 100, 200, 200, fill="#ebedf0", outline="#b0b4c0")
 
         self.canvas.pack()
         self.after(50, self.draw_canvas)
@@ -36,7 +52,7 @@ class App(tk.Frame):
 def main():
     widget = tk.Tk()
     widget.geometry("600x400")
-    widget.title("Random Shape")
+    widget.title("GitHub Image Maker")
     app = App(master=widget)
     app.mainloop()
 
